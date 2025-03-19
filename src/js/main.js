0f3d89a05dbe5f19a45f8a45feabe0355da6c190
Polina <alllrrw@gmail.com>
2025-03-19 22:46:57 +0700

    feat: import component files in main.js, main.scss

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,7 @@
-const message = 'Hello, World!';
-console.log(message);
+import './components/tab';
+import './components/pagination';
+import './components/link';
+import './components/tag';
+import './components/file';
+import './components/alert';
+import './components/button';
