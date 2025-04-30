7936a7dcfa62fe3232cadd20344147fd1a8a1a84
Polina <alllrrw@gmail.com>
2025-04-30 15:01:23 +0700

    feat: add header component

---
+++ src/js/components/header.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggle = document.querySelector('.header__mobile-menu-toggle');
+    const menuList = document.querySelector('.header__mobile-menu-list');
+    const lk = document.querySelector('.lk');
+    
+
+    toggle.addEventListener('click', () => {
+        toggle.classList.toggle('active');
+        menuList.style.display = toggle.classList.contains('active') ? 'flex' : 'none';
+        lk.style.display = toggle.classList.contains('active') ? 'flex' : 'none';
+        document.body.classList.toggle('no-scroll');
+    });
+});
